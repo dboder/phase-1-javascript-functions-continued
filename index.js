135115d3d7d743c0b28567efa5a4adfa589fb996
Dorsey B <dorseybec@gmail.com>
2023-04-15 16:56:42 -0700

    compelte

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(arg ='roller-skate'){
+    return `This Saturday, I want to ${arg}!`;
+}
+function mondayWork(arg = 'go to the office') {
+    return `This Monday, I will ${arg}.`;
+}
+function wrapAdjective(arg = "*"){
+    return function (name = 'special') {
+        return `You are ${arg}${name}${arg}!`;
+    };
+}
+document()
